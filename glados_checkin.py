468dfe0ec570df4a62940b507464131cb3ae9112
Lihao Deng <lihao-deng@outlook.com>
2022-04-27 11:28:00 +0800

    1

--- glados_checkin.py
+++ glados_checkin.py
@@ -1,39 +1,47 @@
-import requests
-import os
 import json
+import logging
+import os
+
+import requests
 
 
 def checkin(cookie, token):
     checkin_url = "https://glados.one/api/user/checkin"
-    
-    headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50",
-    "cookie": cookie}
 
-    data = {"token":"glados_network"}
+    headers = {
+        "user-agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50",
+        "cookie": cookie
+    }
+
+    data = {"token": "glados_network"}
 
     response = requests.post(checkin_url, headers=headers, data=data).text
     dict_response = json.loads(response)
 
-    if dict_response['message'] in ["Checkin! Get 1 Day", "Please Try Tomorrow"]:
+    if dict_response['message'] in [
+            "Checkin! Get 1 Day", "Please Try Tomorrow"
+    ]:
         title = content = "GLADOS打卡成功"
+
+        logging.info("GLADOS打卡成功")
+
     else:
-        content = dict_response['message']                    
+        content = dict_response['message']
         title = "GLADOS打卡失败"
 
-    push_data = {
-    "token":token,
-    "title":title,
-    "content":content
-    }
+        logging.error("GLADOS打卡失败\n" + response)
+
+    push_data = {"token": token, "title": title, "content": content}
 
     push_url = "http://www.pushplus.plus/send/"
 
-    response = requests.post(push_url, data=push_data).text
+    requests.post(push_url, data=push_data).text
 
-    print(response)
-        
 
 def start(event, context):
+    logging.basicConfig(
+        format='%(name)s - %(levelname)s - %(module)s: %(message)s')
     token = os.environ.get('token')
-    cookie =  os.environ.get('cookie')
+    cookie = os.environ.get('cookie')
     checkin(cookie, token)
